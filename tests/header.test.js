61ffba19432097919073705c124b759d54ee8b16
Vlad <kytsevolov@gmail.com>
2022-08-03 17:51:38 -0600

    tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,8 @@
 const pt = require('puppeteer');
+const Buffer = require('safe-buffer').Buffer;
+
+const Keygrip = require('keygrip');
+const keys = require('../config/keys');
 
 let browser;
 let page;
@@ -26,4 +30,34 @@ test('clicking login starts auth flow', async () => {
   const url = await page.url();
 
   expect(url).toMatch(/accounts.google.com/);
+});
+
+test.only('When sign in, shows logout button', async () => {
+  const id = '62dede5c7e66157935e6ac72';
+
+  const sessionObj = {
+    passport: {
+      user: id
+    }
+  };
+
+  const sessionStr = Buffer.from(JSON.stringify(sessionObj)).toString('base64');
+
+  const keygrip = new Keygrip([keys.cookieKey]);
+
+  const sig = keygrip.sign('session=' + sessionStr);
+
+  await page.setCookie({ name: 'session', value: sessionStr });
+  await page.setCookie({ name: 'session.sig', value: sig });
+
+  await page.goto('http://localhost:3000/');
+
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const logoutBtn = await page.$eval(
+    'a[href="/auth/logout"]',
+    el => el.innerHTML
+  );
+
+  expect(logoutBtn).toEqual('Logout');
 });
